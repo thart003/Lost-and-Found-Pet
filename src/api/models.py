3cee438ed6e48372e59ad41be5dd6aaba1999103
Taylor Hart <thart003@ucr.edu>
2022-01-11 18:59:23 +0000

    add Database models

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,11 @@
+import os
+import sys
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eraalchemy import render_er
 
 db = SQLAlchemy()
 
@@ -17,3 +24,36 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250))
+
+
+class Pets(Base):
+    __tablename__ = 'pets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    color = Column(String(250))
+    size = Column(String(250))
+    weight = Column(String(250))
+    location = Column(String(250))
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(ForeignKey('user.id'))
+
+
+class Vetrinarians(Base):
+    __tablename__ = 'vetrinarians'
+    id = Column(Integer, primary_key=True)
+    street_number = Column(String(250))
+    city = Column(String(250))
+    post_code = Column(String(250))
+    phone_number = Column(String(250))
